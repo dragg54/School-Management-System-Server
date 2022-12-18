b5706cc756f8130b6f2af0c6cbb9574cec992d9f
Ajibola Sadiq <ajibola.sadiq@cognitivelabs.co>
2022-12-18 02:07:17 +0100

    initial commit

---
+++ user_auth/views.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from knox.views import LoginView as KnoxLoginView
+from django.shortcuts import render
+from knox.models import AuthToken
+from rest_framework import status, generics, serializers, permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from user_auth.serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if User.objects.filter(**request.data).exists():
+            raise serializers.ValidationError('Student already exists')
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+
+
+@api_view(["GET"])
+def get_current_user(request):
+    current_user_id = request.user.id
+    _current_user_id = AuthToken.objects.filter(user=current_user_id)[:1]
+    for user in _current_user_id:
+        user_data = User.objects.get(username = user.user)
+        serialized_user_data = UserSerializer(user_data)
+        return Response(serialized_user_data.data, status=status.HTTP_200_OK)
